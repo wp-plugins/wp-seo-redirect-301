deccc6a2ed5d428083a2457e012515ee6140c1f3
MMDeveloper <MMDeveloper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-24 06:51:43 +0000

    Fixed posts redirect and deleting redirects.
    git-svn-id: https://plugins.svn.wordpress.org/wp-seo-redirect-301/trunk@629291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo_redirect_301s.php
+++ seo_redirect_301s.php
@@ -3,7 +3,7 @@
 Plugin Name: SEO Redirect 301s
 Plugin URI: http://wordpress.org/extend/plugins/wp-seo-redirect-301/
 Description: Records urls and if a pages url changes, system redirects old url to the updated url.
-Version: 1.4
+Version: 1.5
 Author: Tom Skroza
 License: GPL2
 */
@@ -17,16 +17,16 @@ function register_seo_redirect_301_page() {
 
 add_action( 'save_post', 'save_current_slug' );
 function save_current_slug( $postid ) {
-  global $wpdb;
+	global $wpdb;
   $table_name = $wpdb->prefix . "slug_history";  
   $post_table = $wpdb->prefix . "posts";
   $my_revision = $wpdb->get_row( $wpdb->prepare("SELECT * FROM $post_table WHERE post_type='revision' AND id= %d", $postid) );
   $my_post = $wpdb->get_row( $wpdb->prepare("SELECT * FROM $post_table WHERE post_type IN ('page', 'post') AND id=%d", $my_revision->post_parent) );
   $rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $my_post->ID, 'url' => get_permalink( $my_post->ID )) );
-  $child_pages = get_posts( array('post_type' => 'page','post_parent' => $my_post->ID,'orderby' => 'menu_order'));
-  foreach ($child_pages as $child_page) {
-    $rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $child_page->ID, 'url' => get_permalink( $child_page->ID )) );
-  }
+	$child_pages = get_posts( array('post_type' => 'page','post_parent' => $my_post->ID,'orderby' => 'menu_order'));
+	foreach ($child_pages as $child_page) {
+		$rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $child_page->ID, 'url' => get_permalink( $child_page->ID )) );
+	}
 }
 
 
@@ -46,33 +46,32 @@ add_action( 'template_redirect', 'slt_theme_filter_404', 0 );
 function slt_theme_filter_404() {  
     
     global $wpdb, $wp_query, $post;
-        // Get the name of the current template. 
-        $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
-        if ( $template_name == "") { 
-           // Template is blank, which means page does not exist and is a 404. 
-            $wp_query->is_404 = false;  
-            $wp_query->is_archive = true;  
-            $wp_query->is_post_type_archive = true;  
-            $post = new stdClass();  
-            $post->post_type = $wp_query->query['post_type']; 
+    // Get the name of the current template. 
+    $template_name = get_post_meta( $wp_query->post->ID, '_wp_page_template', true );
     
-           $table_name = $wpdb->prefix . "slug_history";
+    if ( $wp_query->post->ID == "" && $template_name == "") { 
+       // Template is blank, which means page does not exist and is a 404. 
+        $wp_query->is_404 = false;  
+        $wp_query->is_archive = true;  
+        $wp_query->is_post_type_archive = true;  
+        $post = new stdClass();  
+        $post->post_type = $wp_query->query['post_type']; 
+
+       $table_name = $wpdb->prefix . "slug_history";
+       $sql = "SELECT * FROM $table_name where post_id <> 0 AND url='".curPageURL()."/'";
+       $row = $wpdb->get_row($sql);
+
+       $post_table = $wpdb->prefix."posts";
+       $sql = "SELECT * FROM $post_table where ID = ".$row->post_id;
+       $post_row = $wpdb->get_row($sql);
+       if ($post_row->post_type == "post") {
+         wp_redirect(get_option('siteurl')."/?p=".$row->post_id, 301);exit;
+       } else if ($post_row->post_type == "page") {
+         wp_redirect(get_option('siteurl')."/?page_id=".$row->post_id, 301);exit;
+       }
+    }  
     
-           // Look for old url in slug history table and find out the page id.
-           $sql = "SELECT * FROM $table_name where url='".curPageURL()."'";
-            $results=$wpdb->get_results($sql);
-           if ($wpdb->num_rows > 0) {
-             // Now that we have page id, we can now find the current url.
-             foreach ($results as $row){
-               wp_redirect(get_option('siteurl')."/?page_id=".$row->post_id, 301);
-             }
-           } 
-           
-           // TODO: Future functionality. Basically assign a url to be used to redirect to a page of developers choosing.
-           // else if($_SERVER["REQUEST_URI"] == "/wordpress/carsrewrwrwe/") {
-           //              wp_redirect(get_option('siteurl')."/cars", 301);
-           //            }
-        }  
+                   
 }  
 
 function seo_redirect_301_activate() {
@@ -84,8 +83,8 @@ url VARCHAR(255) DEFAULT '' NOT NULL,
 UNIQUE KEY post_id (post_id, url)
 );";
 
-  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-  dbDelta($sql);
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
 
 }
 register_activation_hook( __FILE__, 'seo_redirect_301_activate' );
