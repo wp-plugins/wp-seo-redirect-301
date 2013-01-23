058592795b610dbe912e9d3a36563c5b8cfd34e4
MMDeveloper <MMDeveloper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-23 03:25:54 +0000

    Fixed bug with https urls.
    git-svn-id: https://plugins.svn.wordpress.org/wp-seo-redirect-301/trunk@657205 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo_redirect_301s.php
+++ seo_redirect_301s.php
@@ -3,7 +3,7 @@
 Plugin Name: SEO Redirect 301s
 Plugin URI: http://wordpress.org/extend/plugins/wp-seo-redirect-301/
 Description: Records urls and if a pages url changes, system redirects old url to the updated url.
-Version: 1.6
+Version: 1.6.1
 Author: Tom Skroza
 License: GPL2
 */
@@ -26,16 +26,16 @@ function register_seo_redirect_301_page() {
 
 add_action( 'save_post', 'save_current_slug' );
 function save_current_slug( $postid ) {
-	global $wpdb;
+  global $wpdb;
   $table_name = $wpdb->prefix . "slug_history";  
   $post_table = $wpdb->prefix . "posts";
   $my_revision = $wpdb->get_row( $wpdb->prepare("SELECT * FROM $post_table WHERE post_type='revision' AND id= %d", $postid) );
   $my_post = $wpdb->get_row( $wpdb->prepare("SELECT * FROM $post_table WHERE post_type IN ('page', 'post') AND id=%d", $my_revision->post_parent) );
   $rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $my_post->ID, 'url' => get_permalink( $my_post->ID )) );
-	$child_pages = get_posts( array('post_type' => 'page','post_parent' => $my_post->ID,'orderby' => 'menu_order'));
-	foreach ($child_pages as $child_page) {
-		$rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $child_page->ID, 'url' => get_permalink( $child_page->ID )) );
-	}
+  $child_pages = get_posts( array('post_type' => 'page','post_parent' => $my_post->ID,'orderby' => 'menu_order'));
+  foreach ($child_pages as $child_page) {
+    $rows_affected = $wpdb->insert( $table_name, array( 'post_id' => $child_page->ID, 'url' => get_permalink( $child_page->ID )) );
+  }
 }
 
 
@@ -43,11 +43,7 @@ function curPageURL() {
  $pageURL = 'http';
  if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
  $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
+ $pageURL .= $_SERVER["SERVER_NAME"].preg_replace("/\/$/", "", $_SERVER["REQUEST_URI"]);
  return $pageURL;
 }
 
@@ -68,11 +64,13 @@ function slt_theme_filter_404() {
 
        $table_name = $wpdb->prefix . "slug_history";
        $sql = "SELECT * FROM $table_name where post_id <> 0 AND url='".curPageURL()."/'";
+
        $row = $wpdb->get_row($sql);
 
        $post_table = $wpdb->prefix."posts";
        $sql = "SELECT * FROM $post_table where ID = ".$row->post_id;
        $post_row = $wpdb->get_row($sql);
+
        if ($post_row->post_type == "post") {
          wp_redirect(get_option('siteurl')."/?p=".$row->post_id, 301);exit;
        } else if ($post_row->post_type == "page") {
@@ -92,8 +90,8 @@ url VARCHAR(255) DEFAULT '' NOT NULL,
 UNIQUE KEY post_id (post_id, url)
 );";
 
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-	dbDelta($sql);
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
 
 }
 register_activation_hook( __FILE__, 'seo_redirect_301_activate' );
